a7bd142c8019d7e919e7e2d54599cc6cab0be058
Antolavallee <antoine.lavallee@polymtl.ca>
2021-09-21 11:24:56 -0400

    Exo 5

--- exercice.py
+++ exercice.py
@@ -6,29 +6,71 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number < 0: 
+        number *= -1 
+    return number 
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-
-    return [""]
+    full_name = []
+    for characters in (prefixes):
+        full_name.append(characters + suffixe)
+        
+    return full_name
 
 
 def prime_integer_summation() -> int:
-    return 0
+    total = 0 
+    list_prime = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
+    for number in range(0, len(list_prime)):    
+        total = total + list_prime[number]
+    return total
 
 
 def factorial(number: int) -> int:
-    return 0
+    facto = 1
+    for i in range(1, number + 1):
+       facto = facto * i 
+    return facto 
 
 
 def use_continue() -> None:
-    pass
+    variable = 11 
+    while variable > 0:
+        variable -= 1 
+        if variable == 5: 
+            continue
+        print(variable)
+        
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    boolean_list = []
+    index_integer = 0
+    for index_list in groups: 
+        if len(index_list) > 10 or len(index_list) <= 3: 
+            boolean_list.append(False)
+            continue
+        if len(boolean_list) == index_integer:
+            for age_groups in index_list: 
+                if age_groups == 25: 
+                    boolean_list.append(True)
+                    break
+        if len(boolean_list) == index_integer:
+            for age_groups in index_list: 
+                if age_groups < 18: 
+                    boolean_list.append(False)
+                    break
+        if len(boolean_list) == index_integer:
+            for age_groups in index_list:
+                if age_groups > 70 or age_groups == 50:
+                    boolean_list.append(False)
+                    break
+        else: 
+            boolean_list.append(True)
+        index_integer += 1 
+    return boolean_list
 
 
 def main() -> None:
